adfc83589074321d196c494a26a1689355630917
Pham Quang Binh <phambinh217@gmail.com>
2019-09-15 21:01:20 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="main">
+            <router-view />
+        </div>
+        <div class="copyright">
+            Copyright <a target="_blank" href="https://phambinh.net">phambinh.net</a>
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style src="./assets/scss/style.scss" lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="qr-code">
+        <div class="guide">
+            Scan QRcode below to access this web page.
+        </div>
+        <qr-code :value="url" size="300" class="qr-code__content"></qr-code>
+        <div class="input-group" style="margin-top: 30px;">
+            <input v-model="url" class="qr-code__url input" id="url" />
+            <button class="btn" @click="copy">Copy</button>
+        </div>
+        <div class="message" style="min-height: 13px;">{{ message }}</div>
+    </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    import QrcodeVue from 'qrcode.vue';
+    export default {
+        components: {
+            QrCode: QrcodeVue,
+        },
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+        data () {
+            return {
+                url: 'https://phambinh.net',
+                message: ''
+            }
+        },
+
+        created () {
+            if (process.env.NODE_ENV == 'production') {
+                chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, tabs => {
+                    this.url = tabs[0].url
+                })
+            }
+        },
+
+        methods: {
+            copy () {
+                let copyText = document.getElementById('url')
+                copyText.select()
+                copyText.setSelectionRange(0, 99999)
+                document.execCommand('copy')
+                this.message = 'copied'
+                let vm = this
+                setTimeout(function () {
+                    vm.message = ''
+                }, 2000)
+            }
+        },
+
+        watch: {
+            url () {
+                console.log(url)
+            }
+        }
+    }
 </script>
